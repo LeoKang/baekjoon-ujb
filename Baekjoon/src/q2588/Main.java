2ee8385b6c4b29664c195410183c36838f95a9a6
19-3rsys-004g <19-3rsys-004g@19-3rsys-004>
2023-09-26 16:56:54 +0900

    2588, case 2

--- Baekjoon/src/q2588/Main.java
+++ Baekjoon/src/q2588/Main.java
@@ -6,25 +6,35 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
+//		int num2 = sc.nextInt();
+		String num2 = sc.next();
 		sc.close();
 
 		System.out.println(num1);
 		System.out.println(num2);
-		
+
+		// case 2 -------------------------
+		for (int i = 2; i >= 0; i--) {
+			char c = num2.charAt(i);
+			int ic = Character.getNumericValue(c);
+			System.out.println(num1 * ic);
+		}
+		int ic2 = Integer.parseInt(num2);
+		System.out.println(num1 * ic2);
+
 		// case 1 -------------------------
-		int h = num2 / 100;
-//		System.out.println(h);
-		
-		int t = num2 % 100 / 10;
-//		System.out.println(t);
-		
-		int o = num2 % 10;
-//		System.out.println(o);
-		
-		System.out.println(num1 * o);
-		System.out.println(num1 * t);
-		System.out.println(num1 * h);
-		System.out.println(num1 * num2);
+//		int h = num2 / 100;
+////		System.out.println(h);
+//		
+//		int t = num2 % 100 / 10;
+////		System.out.println(t);
+//		
+//		int o = num2 % 10;
+////		System.out.println(o);
+//		
+//		System.out.println(num1 * o);
+//		System.out.println(num1 * t);
+//		System.out.println(num1 * h);
+//		System.out.println(num1 * num2);
 	}
 }
