e43a286714294b91080786fff2607d0807f5c6b8
19-3rsys-004g <19-3rsys-004g@19-3rsys-004>
2023-10-12 09:56:31 +0900

    9012-2nd case

--- Baekjoon/src/q9012/Main.java
+++ Baekjoon/src/q9012/Main.java
@@ -1,32 +1,64 @@
 package q9012;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
+	// 2nd case
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Stack<Character> stk = new Stack<Character>();
 		int T = sc.nextInt();
 		for (int i = 0; i < T; i++) {
 			String inp = sc.next();
-
-			int cnt = 0;
+			boolean bStr = true;
+			stk.clear();
 			for (int j = 0; j < inp.length(); j++) {
-				if (inp.charAt(j) == '(') {
-					cnt--;
-				} else {
-					cnt++;
-					if(cnt > 0) {
+				if(inp.charAt(j) == '(') {
+					stk.push('(');
+				}else {
+					if(stk.isEmpty()) {
+						bStr = false;
 						break;
+					}else {
+						stk.pop();
 					}
 				}
 			}
-
-			if (cnt == 0) {
+			if(stk.isEmpty() && bStr == true) {
 				System.out.println("YES");
-			} else {
+			}else {
 				System.out.println("NO");
 			}
 		}
 		sc.close();
 	}
+
+	// 1st case
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int T = sc.nextInt();
+//		for (int i = 0; i < T; i++) {
+//			String inp = sc.next();
+//
+//			int cnt = 0;
+//			for (int j = 0; j < inp.length(); j++) {
+//				if (inp.charAt(j) == '(') {
+//					cnt--;
+//				} else {
+//					cnt++;
+//					if(cnt > 0) {
+//						break;
+//					}
+//				}
+//			}
+//
+//			if (cnt == 0) {
+//				System.out.println("YES");
+//			} else {
+//				System.out.println("NO");
+//			}
+//		}
+//		sc.close();
+//	}
 }
